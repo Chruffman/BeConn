ece7539222a62315bd4eb2d3f736f023d7b79585
alko3054 <alko3054@colorado.edu>
2016-10-24 16:58:05 -0600

    Added entries to database, ER Diagram

--- beconn.sql
+++ beconn.sql
@@ -5,14 +5,17 @@ CREATE TABLE IF NOT EXISTS `Event` (
 `Longitude` float NOT NULL,
 `Latitude` float NOT NULL,
 `Host` varchar(40) NOT NULL,
-`Time` DATE NOT NULL,
+`Time` DATETIME NOT NULL,
  `Zip` int(1) NOT NULL,
 
 PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
-INSERT INTO `Event` (`Id`, `Name`, `Address`,`Longitude`,`Latitude`,`Host`,`Time`,`Zip`) VALUES
-(1, 'John Doe', '123 Fake Street', 40.0, 105.0, 'Jane Doe','2016-10-17 02:02:02', 80302),
-(2, 'Billy Bob', '456 Fake Street', 40.0, 105.0, 'JJ', '2016-10-17 02:02:02', 80302);
+INSERT INTO `Event` (`Id`, `Name`, `Address`,`Latitude`,`Longitude`,`Host`,`Time`,`Zip`) VALUES
+(1, 'Soccer Pickup', '6000 Victory Way', 39.805736, -104.891780, 'Jane Doe','2016-10-17 02:02:02', 80022),
+(2, 'Shindig', '909 14th Street', 40.003523, -105.275008, 'John Doe','2016-10-18 03:03:03', 80302),
+(3, 'Ping Pong World Series', '770 29th Street', 40.000613, -105.255289, 'Maestro','2016-10-19 04:04:04', 80303),
+(4, 'Concert', '18300 W Almeda Pkwy', 39.666973, -105.205835, 'JJ','2016-10-20 05:05:05', 80465),
+(5, 'Convention', '700 14th Street', 39.742495, -104.996753, 'Jimmy Neutron','2016-10-21 06:06:06', 80202);
 
 CREATE TABLE IF NOT EXISTS `User` (
 `Id` int(1) NOT NULL auto_increment,
@@ -24,8 +27,11 @@ CREATE TABLE IF NOT EXISTS `User` (
 PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 INSERT INTO `User` (`Id`,`Username`,`Password`,`Email`,`Zip`) VALUES
-(1, 'Jane Doe','password','JaneDoe@dead.com',80302),
-(2, 'JJ','password','JJ@dead.com',80302);
+(1, 'Jane Doe','password1','JaneDoe@dead.com',80302),
+(2, 'John Doe','password2','JohnDoe@dead.com',80302),
+(3, 'Maestro','password3','Maestro@dead.com',80302),
+(4, 'JJ','password4','JJ@dead.com',80302),
+(5, 'Jimmy Neutron','password5','JimmyNeutron@alive.com',80302);
 
 CREATE TABLE IF NOT EXISTS `Location` (
 `Id` int(1) NOT NULL auto_increment,
@@ -37,8 +43,8 @@ CREATE TABLE IF NOT EXISTS `Location` (
 PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 INSERT INTO `Location` (`Id`,`#Events`,`Zip`,`#locals`,`Name`) VALUES
-(1, 1,80302,2,'Boulder');
-
-
-
-
+(1, 1,80022,0,'Commerce City'),
+(2, 1,80302,5,'Boulder'),
+(3, 1,80303,0,'Boulder'),
+(4, 1,80465,0,'Morrison'),
+(5, 1,80202,0,'Denver');
