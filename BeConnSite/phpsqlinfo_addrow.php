4b64931357489a2fe862943294df2f60f1545e2e
David Munson <munsondd@colorado.edu>
2016-11-17 11:52:12 -0700

    change to ajax and jquary

--- BeConnSite/phpsqlinfo_addrow.php
+++ BeConnSite/phpsqlinfo_addrow.php
@@ -2,44 +2,45 @@
 require("phpsqlinfo_dbinfo.php");
 
 // Gets data from URL parameters
-$name = $_GET['name'];
-$address = $_GET['address'];
-$hostName = $_GET['hostName'];
-$lat = $_GET['lat'];
-$lng = $_GET['lng'];
+if(issit($_POST)) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $hostName = $_POST['hostName'];
+    $lat = $_POST['lat'];
+    $lng = $_POST['lng'];
 //$time = $_GET['time'];
-$time = $_GET['2000-01-01']; //temporary var for testing purposes
-$zipcode = $_GET['zipcode'];
-
+    $time = '2000-01-01'; //temporary var for testing purposes
+    $zipcode = '80808';//$_GET['zipcode'];
+}
 // Opens a connection to a MySQL server
-$connection=mysql_connect ("localhost", $username, $password);
-if (!$connection) {
-  die('Not connected : ' . mysql_error());
+$connection=mysqli_connect ($host, $username, $password,$table);
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 // Set the active MySQL database
-$db_selected = mysql_select_db($database, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
+//$db_selected = mysql_select_db($database, $connection);
+//if (!$db_selected) {
+//  die ('Can\'t use db : ' . mysql_error());
+//}
 
 // Insert new row with user data
 $query = sprintf("INSERT INTO Event " .
-         " (Id, Name, Address, Longitude, Latitude, Host, Time, Zip) " .
+         //" (Id, Name, Address, Longitude, Latitude, Host, Time, Zip) " .
          " VALUES (NULL, '%s', '%s', '%s', '%s', '%s','%s','s');",
-         mysql_real_escape_string($name),
-         mysql_real_escape_string($address),
-         mysql_real_escape_string($lng),
-         mysql_real_escape_string($lat),
-         mysql_real_escape_string($hostName),
-         mysql_real_escape_string($time),
-         mysql_real_escape_string($zip));
+         mysqli_real_escape_string($connection,$name),
+         mysqli_real_escape_string($connection,$address),
+         mysqli_real_escape_string($connection,$lng),
+         mysqli_real_escape_string($connection,$lat),
+         mysqli_real_escape_string($connection,$hostName),
+         mysqli_real_escape_string($connection,$time),
+         mysqli_real_escape_string($connection,$zipcode));
 
 
-$result = mysql_query($query);
+$result = mysqli_query($connection,$query);
 
 if (!$result) {
-  die('Invalid query: ' . mysql_error());
+  die('Invalid query: ' . mysqli_error($connection));
 }
 
 ?>
