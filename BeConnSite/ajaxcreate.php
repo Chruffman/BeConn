afffc4e4c7a6ca7d4c329ff58eb9bfdc86139041
David Munson <munsondd@colorado.edu>
2016-12-01 11:53:02 -0700

    completed add row to Event in database

--- BeConnSite/ajaxcreate.php
+++ BeConnSite/ajaxcreate.php
@@ -1,24 +1,20 @@
 <?php
-require("phpsqlinfo_dbinfo.php");
 
-$connection = mysqli_connect ($host, $username, $password,$database); // Establishing Connection with Server..
-
+require 'phpsqlinfo_dbinfo.php';
 //Fetching Values from URL
-$name=$_POST['name'];
-$address=$POST['address'];
-$lng=$POST['lng'];
-$lat=$POST['lat'];
-//for testing purposes
-$time = '2000-01-01';
-$zipcode = 80303;
-//$time=$POST['time'];
-//$zipcode=$POST['zipcode'];
-//Insert query
-//$query = mysqli_query($connection,"insert into Event(Name,Address,Longitude,Latitude,Host,Time,Zip)
-//    values ('$name','$address','$lng','$lat','$time','$zipcode')");
+$name = $_POST['name'];
+$address = $_POST['address'];
+$lng = $_POST['lng'];
+$lat = $_POST['lat'];
+$hostname = $_POST['hostname'];
 
-//testing query
-$query = mysqli_query($connection,"insert into Event(Name,Address,Longitude,Latitude,Host,Time,Zip) values ('anevent','103','-103.4403','40.0000','host','2000-01-01',80542);");
-echo "Form Submitted Succesfully";
+$time= $_POST['time'];
+$zipcode=$_POST['zipcode'];
+
+//Insert query
+$connection = new  mysqli( $host, $username,$password,$database); // Establishing Connection with Server..
+$query = mysqli_query($connection,"insert into Event(Name,Address,Longitude,Latitude,Host,Time,Zip) values ('$name','$address','$lng','$lat','$hostname','$time','$zipcode')");
+
+echo "Event Created  Succesfully";
 mysqli_close($connection); // Connection Closed
 ?>
